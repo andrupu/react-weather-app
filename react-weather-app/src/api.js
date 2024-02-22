2e4f1c8c974baa39e78a17a44b852a6278de80be
Andrupu <47467951+andrupu@users.noreply.github.com>
2024-02-21 22:52:14 -0500

    build city search component

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '14b18f41e5mshff457fd98b8630ap196c93jsn3b8336e81b40',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
