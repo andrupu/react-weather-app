9687a71497829f93b4b2bfaefa65c66a98779253
Andrupu <47467951+andrupu@users.noreply.github.com>
2024-02-22 17:35:18 -0500

    fetch data from weather API

--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -9,3 +9,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f98cefab0c0db48346783fe6616132aa";
